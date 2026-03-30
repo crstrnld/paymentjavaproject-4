a84108713c22fe1ad4b3e5f0cca5ba5af28fb71d
crstrnld <cristianokece@gmail.com>
2026-03-30 10:50:45 +0700

    Update PaymentSystem.java

--- PaymentSystem.java
+++ PaymentSystem.java
@@ -2,9 +2,9 @@ import java.util.ArrayList;
 
 public class PaymentSystem {
     public static void main(String[] args) {
-        Payment p1 = new CreditCardPayment("Alice", 1000f, 10, "1234-5678-9876"); // diskon 10%
-        Payment p2 = new PayPalPayment("Bob", 500f, "bob@example.com");           // tanpa diskon
-        Payment p3 = new CryptoPayment("Charlie", 750f, 20, "0xABC123XYZ");       // diskon 20%
+        Payment p1 = new CreditCardPayment("Alice", 1000f, 10, "1234-5678-9876"); 
+        Payment p2 = new PayPalPayment("Bob", 500f, "bob@example.com");           
+        Payment p3 = new CryptoPayment("Charlie", 750f, 20, "0xABC123XYZ");      
 
         ArrayList<Payment> payments = new ArrayList<>();
         payments.add(p1);
